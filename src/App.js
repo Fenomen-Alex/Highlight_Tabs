5e82c4ccff9735ddfd49dfb12e0be875148c0d9d
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-08 23:42:32 +0300

    Applying highlight

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+const Tab = ({title}) => {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  const moveHighlight = (e) => {
+    setHighlightStyle({ left: e.nativeEvent.layerX - 150});
+  }
+
+  const hideHighlight = (e) => {
+    setHighlightStyle({ left: e.nativeEvent.layerX - 150, opacity: 0 })
+  }
+  return (
+    <div
+      className="tab"
+      onMouseMove={moveHighlight}
+      onMouseOut={hideHighlight}
+    >
+      <div
+        className="highlight"
+        style={highlightStyle}
+      />
+      <a>{title}</a>
+    </div>
+  )
+}
+
 function App() {
+
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
+          <Tab title="Home" />
+          <Tab title="About" />
+          <Tab title="Features" />
         </div>
 
         <div className="viewport">Pages Go Here</div>
